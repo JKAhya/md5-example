7a9b2f20cf555f2112c0e7bf6351d311bd40f10e
JKI <xjdius37@gmail.com>
2025-10-26 20:07:31 +0900

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,65 +1,52 @@
-import Image from "next/image";
+"use client";
+
+import { useState } from "react";
+const md5 = require("md5");
 
 export default function Home() {
+  const [userText, setUserText] = useState("");
+  const [results, setResults] = useState<string[]>([]);
+
+  function transfer() {
+    if (userText.trim() !== "") {
+      setResults((prev) => [...prev, md5(userText)]);
+      setUserText("");
+    }
+  }
+
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <div>
+      <div className="text-5xl nh text-center mt-35">암호 함수</div>
+      <div className="w-full flex justify-center items-center">
+        <div className="my-35 w-[95%] rounded-4xl nh text-center py-20 bg-gray-100">
+          <div className="nh text-xl">
+            암호 함수를 통해 변환할&nbsp;
+            <wbr />
+            글을 입력해주세요.
+          </div>
+          <input
+            value={userText}
+            onChange={(e) => setUserText(e.target.value)}
+            type="text"
+            placeholder="변환할 글 입력"
+            className="bg-gray-200 w-[70%] mt-3 h-12 rounded-4xl px-3"
+          ></input>
+          <button
+            onClick={transfer}
+            className="ml-2 rounded-3xl w-[15%] bg-orange-300 nh text-gray-950 h-12"
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+            변환
+          </button>
+          <div className="nh text-start w-[85%] rounded-3xl mt-5 px-5 bg-gray-200 justify-self-center flex-none">
+            <div className="pt-2 pb-2" id="result">
+              변환 기록
+              {results.map((hash, idx) => (
+                <div key={idx}>{hash}</div>
+              ))}
+            </div>
+          </div>
         </div>
-      </main>
+      </div>
     </div>
   );
 }
